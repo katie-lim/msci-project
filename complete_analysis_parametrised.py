6113ae98a46295df83af7a83be0ef1cce5cd3011
katie-lim <>
2023-11-27 17:42:13 +0000

    edit complete_analysis_parametrised.py, add W and SN data

--- complete_analysis_parametrised.py
+++ complete_analysis_parametrised.py
@@ -42,10 +42,17 @@ z_true = true_z_of_r(radii_true)
 
 k_bin_edges, k_bin_heights = create_power_spectrum(200, 10, np.array([0.1, 0.35, 0.6, 0.8, 0.9, 1, 0.95, 0.85, 0.7, 0.3]))
 
-k_vals = np.linspace(0, 400, 1000)
+k_vals = np.linspace(0, 275, 5000)
 P_vals = [P_parametrised(k, k_bin_edges, k_bin_heights) for k in k_vals]
 
-plt.plot(k_vals, P_vals)
+# #1071E5
+plt.plot(k_vals, P_vals, c="k", lw=1.25)
+plt.xlim(0)
+plt.ylim(0)
+plt.xlabel("$k$")
+plt.title("$P(k)$")
+plt.tight_layout()
+# plt.savefig("thesis/plots/power_spectrum_10_bins.svg")
 plt.show()
 
 def P(k):
@@ -88,24 +95,24 @@ f_lmn_0 = calc_f_lmn_0_numba(radii_fiducial, all_observed_grids, l_max, k_max, n
 
 
 # Save coefficients to a file for future use
-saveFileName = "data/f_lmn_0_true-%.3f_fiducial-%.3f_l_max-%d_k_max-%.2f_r_max_true-%.3f_R-%.3f_P-parametrised-2023-04-20-10-bins.npy" % (omega_matter_true, omega_matter_0, l_max, k_max, r_max_true, R)
+saveFileName = "data/f_lmn_0_true-%.3f_fiducial-%.3f_l_max-%d_k_max-%.2f_r_max_true-%.3f_R-%.3f_P-parametrised-2023-11-27-10-bins.npy" % (omega_matter_true, omega_matter_0, l_max, k_max, r_max_true, R)
 np.save(saveFileName, f_lmn_0)
 print("Done! File saved to", saveFileName)
 
 # %%
 
 # Or, load f_lmn_0 from a file
-omega_matter_true = 0.315
-omega_matter_0 = 0.315
-l_max = 15
-k_max = 200
-r_max_true = 0.75
-R = 0.25
-P_amp = 1
+# omega_matter_true = 0.315
+# omega_matter_0 = 0.315
+# l_max = 15
+# k_max = 200
+# r_max_true = 0.75
+# R = 0.25
+# P_amp = 1
 
-saveFileName = "data/f_lmn_0_true-%.3f_fiducial-%.3f_l_max-%d_k_max-%.2f_r_max_true-%.3f_R-%.3f_P-amp_%.2f.npy" % (omega_matter_true, omega_matter_0, l_max, k_max, r_max_true, R, P_amp)
+# saveFileName = "data/f_lmn_0_true-%.3f_fiducial-%.3f_l_max-%d_k_max-%.2f_r_max_true-%.3f_R-%.3f_P-amp_%.2f.npy" % (omega_matter_true, omega_matter_0, l_max, k_max, r_max_true, R, P_amp)
 
-saveFileName = "data/f_lmn_0_true-0.315_fiducial-0.315_l_max-15_k_max-200.00_r_max_true-0.750_R-0.250_P-parametrised-2023-04-20-10-bins.npy"
+saveFileName = "data/f_lmn_0_true-0.315_fiducial-0.315_l_max-15_k_max-200.00_r_max_true-0.750_R-0.250_P-parametrised-2023-11-27-10-bins.npy"
 
 f_lmn_0 = np.load(saveFileName)
 
@@ -139,7 +146,7 @@ for omega_matter in omega_matters:
 
 
 # Compute shot noise
-SN_saveFileName = "data/SN_no_tayl_exp_zeros_omega_m-%.5f_omega_m_0-%.5f_l_max-%d_k_max-%.2f_r_max_0-%.4f_R-%.3f.npy" % (omega_matter, omega_matter_0, l_max, k_max, r_max_0, R)
+SN_saveFileName = "data/SN_no_tayl_exp_zeros_omega_m_0-%.5f_l_max-%d_k_max-%.2f_r_max_0-%.4f_R-%.3f.npy" % (omega_matter_0, l_max, k_max, r_max_0, R)
 
 if path.exists(SN_saveFileName):
     SN = np.load(SN_saveFileName)
@@ -213,14 +220,14 @@ with Pool() as pool:
     sampler = emcee.EnsembleSampler(
         nwalkers, ndim, log_probability, pool=pool, backend=backend
     )
-    sampler.run_mcmc(pos, 25000, progress=True);
+    sampler.run_mcmc(pos, 10000, progress=True)
 
 
 # %%
 
 fig, axes = plt.subplots(11, figsize=(10, 7), sharex=True)
 samples = sampler.get_chain()
-labels = ["$\Omega_m$", *["$P_%d$" % (i+1) for i in range(10)]]
+labels = ["$\Omega_m$", *["$P_{%d}$" % (i+1) for i in range(10)]]
 for i in range(ndim):
     ax = axes[i]
     ax.plot(samples[:, :, i], "k", alpha=0.3)
@@ -228,7 +235,7 @@ for i in range(ndim):
     ax.set_ylabel(labels[i])
     ax.yaxis.set_label_coords(-0.1, 0.5)
 
-axes[-1].set_xlabel("step number");
+axes[-1].set_xlabel("step number")
 
 # %%
 
@@ -247,7 +254,86 @@ print(flat_samples.shape)
 fig = corner.corner(
     flat_samples, labels=labels, truths=[0.315, *[0.1, 0.35, 0.6, 0.8, 0.9, 1, 0.95, 0.85, 0.7, 0.3]]
     # flat_samples, labels=labels, truths=[0.315, *[0.35, 0.85, 1, 0.85, 0.35]]
-);
+)
 
+
+# %%
+
+# Or, load samples from .h5 file
+filename = "data/parametrised_power_spectrum_10_bins.h5"
+reader = emcee.backends.HDFBackend(filename)
+
+tau = reader.get_autocorr_time()
+burnin = int(2 * np.max(tau))
+thin = int(0.5 * np.min(tau))
+# samples = reader.get_chain(discard=burnin, flat=True, thin=thin)
+samples = reader.get_chain(discard=10000, flat=True)
+log_prob_samples = reader.get_log_prob(discard=burnin, flat=True, thin=thin)
+
+print("burn-in: {0}".format(burnin))
+print("thin: {0}".format(thin))
+print("flat chain shape: {0}".format(samples.shape))
+print("flat log prob shape: {0}".format(log_prob_samples.shape))
+
+# %%
+
+# # Violin plot
+
+# powerSpectrumSamples = samples[:, 1:] # Ignore first column, which is omega_matter
+
+# plt.figure(dpi=200, figsize=(8, 6))
+# violin_plot = plt.violinplot(powerSpectrumSamples, showmeans=False)
+
+# plt.plot([i+1 for i in range(10)], [0.1, 0.35, 0.6, 0.8, 0.9, 1, 0.95, 0.85, 0.7, 0.3], "o", label="Truth", c="k")
+
+# k_vals = np.linspace(0, 201, 5000)
+# P_vals = [P_parametrised(k, k_bin_edges, k_bin_heights) for k in k_vals]
+# plt.plot(k_vals * (10/200) + 0.5, P_vals, c="#003a66", zorder=0, lw=1, alpha=0.3)
+
+
+# color = "#20A285"
+# color_fill = "#1071E5"
+
+# for violin in violin_plot['bodies']:
+#     violin.set_color(color_fill)
+#     violin.set_alpha(0.6)
+
+# # parts = ["cmins", "cmaxes", "cmeans", "cbars"]
+# # for part in parts:
+# #     violin_plot[part].set_color(color)
+
+# handles, labels = plt.gca().get_legend_handles_labels()
+# handles.insert(0, violin_plot["bodies"][0])
+# labels.insert(0, "Samples")
+# plt.legend(handles, labels)
+# plt.xticks([i+1 for i in range(10)], [i+1 for i in range(10)])
+# plt.xlabel("$i$")
+# plt.ylabel("$P_{i}$")
+# plt.ylim(0)
+# plt.xlim(0.5)
+# # plt.savefig("thesis/plots/violin_plot.svg")
+# plt.show()
+
+# %%
+
+# import pymc
+# # from scipy.integrate import simpson
+# # simpson(counts, bin_mids)
+
+# edgecolor = "#1071E5"
+# facecolor = "#1071E5"
+# counts, bins, bars = plt.hist(np.transpose(samples)[0], bins=50, density=True, color=facecolor, edgecolor=edgecolor, linewidth=0.8, alpha=0.4)
+# bin_mids = [(bins[i] + bins[i+1]) / 2 for i in range(len(bins) - 1)]
+
+# hpdi = pymc.stats.hdi(np.transpose(samples)[0], hdi_prob=0.68)
+# plt.vlines(hpdi, 0, np.max(counts), color="k", linestyle="dashed", linewidth=1, label="68% HPDI")
+
+# plt.xlabel("$\Omega_m$")
+# plt.title("$p(\Omega_m)$")
+# # plt.savefig("thesis/plots/omega_matter_posterior_10_bins.svg")
+# plt.show()
+
+# peak_omega_matter = bin_mids[np.argmax(counts)]
+# print("Ωₘ = %.5f +%.5f -%.5f" % (peak_omega_matter, hpdi[1] - peak_omega_matter, peak_omega_matter - hpdi[0]))
 
 # %%
